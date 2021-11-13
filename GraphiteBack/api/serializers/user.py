bd281c44d79d840a910b5a333280518eb3088fc5
tehno <tehnolog1999@gmail.com>
2021-11-13 11:19:48 +1000

    api changes

--- GraphiteBack/api/serializers/user.py
+++ GraphiteBack/api/serializers/user.py
@@ -59,8 +59,8 @@ class CurrentUserSerializer(StatsSerializer):
         if instance.is_superuser:
             instance.is_superuser = validated_data.get('is_superuser', instance.is_superuser)
             instance.is_staff = validated_data.get('is_staff', instance.is_staff)
-            instance.user_permissions = validated_data.get('user_permissions', instance.user_permissions)
-            instance.groups = validated_data.get('groups', instance.groups)
+            instance.user_permissions.set(validated_data.get('user_permissions', instance.user_permissions))
+            instance.groups.set(validated_data.get('groups', instance.groups))
 
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
