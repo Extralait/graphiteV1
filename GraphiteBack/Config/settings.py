1401952628a9e78d3ea6e1c00d5d4f02a239bfa6
atknin <ivan@atknin.ru>
2021-11-21 20:36:58 +0300

    e

--- GraphiteBack/Config/settings.py
+++ GraphiteBack/Config/settings.py
@@ -69,7 +69,8 @@ MIDDLEWARE = [
 
 # Путь к главным URL
 ROOT_URLCONF = 'Config.urls'
-
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 # Настройка шаблонизатора
 TEMPLATES = [
     {
